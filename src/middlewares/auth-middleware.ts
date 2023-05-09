9b737bb883c4f9fe81d5b44e27d563d352727d0f
Pedro Rondelli <pedrorondelli@hotmail.com>
2023-05-09 15:20:34 -0300

    refact: types the request body inside the middleware

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -1,24 +1,29 @@
 import { Request, Response, NextFunction } from "express";
 import { signInSchema, signUpSchema } from "../schemas/auth.schemas";
+import { Registration } from "../protocols";
 
 function authLoginMiddleware(req: Request, res: Response, next: NextFunction) {
-  const { body } = req;
+  const  body  = req.body as Omit<Registration, "confirmation">;
   const validation = signInSchema.validate(body, { abortEarly: false });
 
   if (validation.error) {
-   return res.status(400).send(validation.error.details);
+    return res.status(400).send(validation.error.details);
   }
 
   next();
 }
 
-function authRegistrationMiddleware(req: Request, res: Response, next: NextFunction){
-  const { body } = req;
+function authRegistrationMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const  body  = req.body as Registration;
 
   const validation = signUpSchema.validate(body, { abortEarly: false });
 
   if (validation.error) {
-   return res.status(400).send(validation.error.details);
+    return res.status(400).send(validation.error.details);
   }
 
   next();
@@ -26,5 +31,5 @@ function authRegistrationMiddleware(req: Request, res: Response, next: NextFunct
 
 export const authMiddlewares = {
   authLoginMiddleware,
-  authRegistrationMiddleware
+  authRegistrationMiddleware,
 };
