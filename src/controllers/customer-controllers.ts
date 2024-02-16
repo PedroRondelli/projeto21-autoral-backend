64541a3aac40b6c7227501e17886ad84d362c237
PedroRondelli <pedrorondelli@hotmail.com>
2024-02-16 10:02:08 -0300

    auteração tsconfig

--- src/controllers/customer-controllers.ts
+++ src/controllers/customer-controllers.ts
@@ -2,9 +2,9 @@ import { Request, Response } from "express";
 import { customerService } from "../service/customer-service";
 
 async function getAllProfiles(req: Request, res: Response) {
-  try {
+    try {
     const allArtistProfile = await customerService.getAllProfiles();
-    res.status(200).send(allArtistProfile);
+        res.status(200).send(allArtistProfile);
   } catch (error) {
     console.log(error);
     res.sendStatus(400);
